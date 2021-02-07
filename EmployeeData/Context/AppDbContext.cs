c81fd3906699028fb77f807b977227823b72ff1c
Morshed Sarwer <morshedsarwer@gmail.com>
2021-02-07 14:28:44 +0600

    database connetction ready

---
+++ EmployeeData/Context/AppDbContext.cs
@@ -0,0 +1,15 @@
+using EmployeeData.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeData.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+            
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ EmployeeData/Models/Employee.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeData.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
